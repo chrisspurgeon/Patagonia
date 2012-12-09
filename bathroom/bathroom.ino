27d220375d12ab3d98140e328310c17bed40fd0d
Chris Spurgeon <chris@spurgeonworld.com>
2012-12-08 16:39:09 -0800

    Basic function done. Cake.

--- bathroom/bathroom.ino
+++ bathroom/bathroom.ino
@@ -1,1 +1,23 @@
 
+long changeTime;
+
+
+void setup() {
+  pinMode(11, INPUT);
+  pinMode(8, OUTPUT); //open
+  pinMode(9, OUTPUT); // occupied
+  Serial.begin(9600);
+  
+  
+}
+
+void loop() {
+  Serial.println(digitalRead(11));
+  if (digitalRead(11) == HIGH) {
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+  } else {
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);  
+  }
+}
