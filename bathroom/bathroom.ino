8365f1a8c7346642f1a1cb5f1bff93ecf9a20856
Chris Spurgeon <chris@spurgeonworld.com>
2012-12-08 16:52:36 -0800

    Added a five second delay.

--- bathroom/bathroom.ino
+++ bathroom/bathroom.ino
@@ -1,5 +1,7 @@
 
-long changeTime;
+unsigned long offTime;
+bool lastStatus;
+long delayTime = 5000;
 
 
 void setup() {
@@ -7,17 +9,28 @@ void setup() {
   pinMode(8, OUTPUT); //open
   pinMode(9, OUTPUT); // occupied
   Serial.begin(9600);
-  
-  
+
+
 }
 
 void loop() {
-  Serial.println(digitalRead(11));
   if (digitalRead(11) == HIGH) {
     digitalWrite(8, LOW);
     digitalWrite(9, HIGH);
-  } else {
-    digitalWrite(8, HIGH);
-    digitalWrite(9, LOW);  
+    lastStatus = true;
+  } 
+  else {
+    if (lastStatus == true) {
+      Serial.println("OFF!");
+      offTime = millis();
+      lastStatus = false;
+    }
+    if (abs(millis() - offTime) > delayTime) {
+      Serial.println("OK to turn on light!");
+      digitalWrite(8, HIGH);
+      digitalWrite(9, LOW);  
+    }
   }
 }
+
+
